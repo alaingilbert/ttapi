bdb52a4efb038003dd676557c74a64084998b70c
Alain Gilbert <alain.gilbert.15@gmail.com>
2024-08-19 11:00:05 -0700

    new domain is actually deepcut.live

--- constants.go
+++ constants.go
@@ -1,8 +1,8 @@
 package ttapi
 
 const (
-	wsOrigin   = "https://deepcut.fm"
-	wssURL     = "wss://chat1.deepcut.fm:8080/socket.io/websocket"
+	wsOrigin   = "https://deepcut.live"
+	wssURL     = "wss://chat1.deepcut.live:8080/socket.io/websocket"
 	wsProtocol = ""
 
 	bootedUser     = "booted_user"
